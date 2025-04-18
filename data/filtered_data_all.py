55a8299419bf26852ce868bf2ff489c38f4fe3a0
Jiamu Luo <130644696+Ljia1009@users.noreply.github.com>
2025-04-18 15:33:16 -0700

    Add files via upload

---
+++ data/filtered_data_all.py
@@ -0,0 +1,21 @@
+import json
+
+file_path = 'ORSUM_train.jsonl'
+data_list = []
+
+with open(file_path, 'r') as f:
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+        data = json.loads(line)
+        extracted_item = {'Review': [], 'Metareview': []}
+        key_to_exclude = ['title']
+        for review in data['Review']:
+            extracted_item['Review'].append(
+                {k: v for k, v in review.items() if k not in key_to_exclude})
+            if 'Metareview' in data:
+                extracted_item['Metareview'].append(data['Metareview'])
+        data_list.append(extracted_item)
+
+print(data_list[:1])
