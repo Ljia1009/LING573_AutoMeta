f1c2eedfb34cbeda5227ac2b3a30ecf0c49e2516
Ghau-Yan Ngae <ngaeghy@ymail.ne.jp>
2025-04-21 23:18:46 -0700

    Extract metareview once per paper, if it exists

--- data/filtered_data_all.py
+++ data/filtered_data_all.py
@@ -11,9 +11,10 @@ with open(file_path, 'r') as f:
         data = json.loads(line)
         extracted_item = {'Review': [], 'Metareview': ''}
         key_to_contain = ['review', 'rating', 'confidence']
-        for review in data['Review']:
-            if 'review' in review and 'Metareview' in data:
-                extracted_item['Review'].append(
-                    {k: v for k, v in review.items() if k in key_to_contain and k in review})
-                extracted_item['Metareview'] = data['Metareview']
-        data_list.append(extracted_item)
+        if 'Metareview' in data:
+            extracted_item['Metareview'] = data['Metareview']
+            for review in data['Review']:
+                if 'review' in review:
+                    extracted_item['Review'].append(
+                        {k: v for k, v in review.items() if k in key_to_contain and k in review})
+            data_list.append(extracted_item)
