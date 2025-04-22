23b16bfcfbe65e0b05d63ab950012739e1947c88
Ghau-Yan Ngae <ngaeghy@ymail.ne.jp>
2025-04-21 23:18:46 -0700

    Extract metareview once per paper, if it exists

--- data/flitered_data_review_only.py
+++ data/flitered_data_review_only.py
@@ -10,8 +10,9 @@ with open(file_path, 'r') as f:
             continue
         data = json.loads(line)
         extracted_item = {'Review': [], 'Metareview': ''}
-        for review in data['Review']:
-            if 'Metareview' in data and 'review' in review:
-                extracted_item['Metareview'] = data['Metareview']
-                extracted_item['Review'].append(review['review'])
-        data_list.append(extracted_item)
+        if 'Metareview' in data:
+            extracted_item['Metareview'] = data['Metareview']
+            for review in data['Review']:
+                if 'review' in review:
+                    extracted_item['Review'].append(review['review'])
+            data_list.append(extracted_item)
