7901e65e547535cdd6af1831b7248e12f3b8ec71
Ljia1009 <130644696+Ljia1009@users.noreply.github.com>
2025-04-27 16:29:20 -0700

    modified:   src/models/baseline/pegasus_x.py

--- src/models/baseline/pegasus_x.py
+++ src/models/baseline/pegasus_x.py
@@ -1,27 +1,30 @@
 from transformers import AutoTokenizer, PegasusForConditionalGeneration
 import torch
-def run_pegasus_x_summarization(sample_size:int, data_list:list):
+
+
+def run_pegasus_x_summarization(sample_size: int, data_list: list):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = PegasusForConditionalGeneration.from_pretrained(
-    "google/pegasus-x-base").to(device)
+        "google/pegasus-x-base").to(device)
     tokenizer = AutoTokenizer.from_pretrained("google/pegasus-x-base")
     output = []
     gold_metareview = []
     for review in data_list[:sample_size]:
         gold_metareview.append(review['Metareview'])
-    result = 'Below are multiple summaries of a paper\'s reviews. '
+    # result = 'Below are multiple summaries of a paper\'s reviews. '
     if not sample_size:
         sample_size = len(data_list)
     for paper in data_list[:sample_size]:
+        result = 'Below are multiple summaries of a paper\'s reviews. '
         for review in paper['ReviewList']:
             inputs = tokenizer(review,
-                            max_length=500, return_tensors="pt")
+                               max_length=500, return_tensors="pt")
             summary_ids = model.generate(inputs["input_ids"])
             summary = tokenizer.batch_decode(
                 summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
             result += summary+'\n'
         final = tokenizer(result,
-                        max_length=500, return_tensors="pt")
+                          max_length=500, return_tensors="pt")
         meta_summary_ids = model.generate(final["input_ids"])
         meta_sum = tokenizer.batch_decode(
             meta_summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
